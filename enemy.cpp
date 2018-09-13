ba015598ab874cb75abfe4d0e58f1de8a4725c1c
banadora <banadora@tuta.io>
2018-09-13 16:46:43 +0200

    simple animation on/off on enemy's attack

--- enemy.cpp
+++ enemy.cpp
@@ -12,6 +12,9 @@ extern xGame *game;
 xEnemy::xEnemy(QString enemyName) :
     xCharacter(enemyName)
 {
+    animView = new QGraphicsPixmapItem;
+    animView->setZValue(14);
+
     //connect timers
     moveTimer = new QTimer;
     connect(moveTimer, SIGNAL(timeout()), this, SLOT(randMove()));
@@ -19,20 +22,28 @@ xEnemy::xEnemy(QString enemyName) :
 
     attackTimer = new QTimer;
     connect(attackTimer, SIGNAL(timeout()), this, SLOT(attack()));
+
+    animTimer = new QTimer;
+    connect(animTimer, SIGNAL(timeout()), this, SLOT(animation()));
 }
 
-double xEnemy::getDistance() {
+QLineF xEnemy::getDistanceLine() {
     QPointF enemyPos(getView()->pos().x() + PixelsX/2, getView()->pos().y() + PixelsY/2);
     QPointF heroPos(game->hero->getView()->pos().x() + PixelsX/2, game->hero->getView()->pos().y() + PixelsY/2);
     QLineF ln(enemyPos, heroPos);
-    return ln.length();
+    return ln;
 }
 
 void xEnemy::randMove() {
     //check distance between enemy and hero then stop moving and switch to attack if hero is close
-    double d = getDistance();
+    double d = getDistanceLine().length();
     if (d < 36) {
         moveTimer->stop();
+
+
+        game->scene->addItem(animView);
+        animTimer->start(450);
+
         attackTimer->start(1000);
         qDebug()<< "attacked!";
         return;
@@ -55,9 +66,13 @@ void xEnemy::randMove() {
 
 void xEnemy::attack() {
     //check distance between enemy and hero then stop attacking and switch to moving if hero is far
-    double d = getDistance();
+    double d = getDistanceLine().length();
     if (d > 36) {
         attackTimer->stop();
+
+        game->scene->removeItem(animView);
+        animTimer->stop();
+
         moveTimer->start(800);
         qDebug()<< "moving!";
         return;
@@ -65,4 +80,17 @@ void xEnemy::attack() {
 
     game->hero->getDamaged(2);
     qDebug() << "pv: " + QString::number(game->hero->getHP());
+}
+
+void xEnemy::animation() {
+    if (animToken == 1) {
+        animView->setPixmap(QPixmap(":/img/claw0"));
+        animView->setPos(getDistanceLine().center().x()-11, getDistanceLine().center().y()-6);
+        animToken = 0;
+    }
+    else {
+        animView->setPixmap(QPixmap(""));
+        animView->setPos(getDistanceLine().center().x()-11, getDistanceLine().center().y()-6);
+        animToken = 1;
+    }
 }
--- enemy.h
+++ enemy.h
@@ -9,17 +9,23 @@ class xEnemy : public QObject, public xCharacter {
 
     Q_OBJECT
 
+    QGraphicsPixmapItem *animView;
+
     QTimer *moveTimer;
     QTimer *attackTimer;
+    QTimer *animTimer;
+
+    bool animToken = 1;
 
 public:
     xEnemy(QString enemyName="unknown");
 
-    double getDistance();
+    QLineF getDistanceLine();
 
 public slots:
     void randMove();
     void attack();
+    void animation();
 };
 
 #endif // ENEMY_H
